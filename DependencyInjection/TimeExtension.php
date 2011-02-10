514b6b91ca39fd21d4ce132eb42393443fdd5d20
excelwebzone <hagaiarieli@Hagai-Arielis-MacBook-Pro.local>
2011-02-09 16:51:31 -0800

    Load twig.xml

--- DependencyInjection/TimeExtension.php
+++ DependencyInjection/TimeExtension.php
@@ -26,6 +26,7 @@ class TimeExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('templating.xml');
+        $loader->load('twig.xml');
     }
 
     /**
