239707140465f7950b4fbf7684113da79833bcad
excelwebzone <hagaiarieli@Hagai-Arielis-MacBook-Pro.local>
2011-03-14 08:11:23 -0700

    Refactored DI Extension

--- DependencyInjection/EWZTimeExtension.php
+++ DependencyInjection/EWZTimeExtension.php
@@ -9,43 +9,16 @@ use Symfony\Component\Config\FileLocator;
 
 class EWZTimeExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        foreach ($configs as $config) {
-            $this->doConfigLoad($config, $container);
-        }
-    }
-
     /**
-     * Loads the time configuration.
+     * Loads the configuration.
      *
-     * @param array            $config    An array of configuration settings
+     * @param array            $configs   An array of configuration settings
      * @param ContainerBuilder $container A ContainerBuilder instance
      */
-    protected function doConfigLoad(array $config, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('templating.xml');
         $loader->load('twig.xml');
-    }
-
-    /**
-     * Returns the base path for the XSD files.
-     *
-     * @return string The XSD base path
-     */
-    public function getXsdValidationBasePath()
-    {
-        return __DIR__.'/../Resources/config/schema';
-    }
-
-    /**
-     * Returns the namespace to be used for this extension (XML namespace).
-     *
-     * @return string The XML namespace
-     */
-    public function getNamespace()
-    {
-        return 'http://symfony.com/schema/dic/ewz/time';
     }
 }
